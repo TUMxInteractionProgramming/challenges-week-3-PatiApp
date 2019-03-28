954e2304fd6337ad67f21021998f24723708329d
PatiApp <48488374+PatiApp@users.noreply.github.com>
2019-03-28 22:35:18 +0100

    challenge 6

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
 console.log('app is alive')
 
 function switchChannel(channelName, channelLoc='upgrading.never.helps') {
-    console.log('Tuning into channel' + channelName);
+    console.log('Tuning into channel ' + channelName);
     document.getElementById('channel-name').innerHTML = channelName;
     document.getElementById('location-name').innerHTML = channelLoc;
     document.getElementById('channel-location').href = 'https://w3w.co/' + channelLoc;
     uncheckStar();
+    $('#channels ul li.selected').removeClass('selected');
+    $('#channels ul li:contains(' + channelName + ')').addClass('selected');
 }
 
 function checkStar() {
@@ -14,4 +16,14 @@ function checkStar() {
 
 function uncheckStar() {
     $('#chat .channel-meta img').attr ('src', 'http://ip.lfe.mw.tum.de/sections/star-o.png');
+}
+
+function selectTab(tabId) {
+    $('#tab-bar .selected').removeClass('selected');
+    $('#tab-bar #' + tabId).addClass('selected');
+    console.log('Changing to tab ' + tabId);
+}
+
+function toggleEmojis() {
+    $('#emojis').toggle();
 }
